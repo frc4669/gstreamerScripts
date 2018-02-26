e95a505f6b10209463ad05c641a0ff589085ff9d
Khanh Hoang <dextinfire@live.com>
2018-02-25 19:46:26 -0800

    Update Server2CamStream.sh

--- Server2CamStream.sh
+++ Server2CamStream.sh
@@ -1,14 +1,13 @@
-
-gst-launch-1.0 videomixer name=mix \
+gst-launch-1.0 -e -v videomixer name=mix \
  sink_0::xpos=0   sink_0::ypos=0 \
  sink_1::xpos=0   sink_1::ypos=480 \
-! queue ! omxh264enc control-rate=2 bitrate=400000 ! 'video/x-h264, stream-format=(string)byte-stream' \
-! h264parse config-interval=1 \
-! rtph264pay mtu=1400 \
-! udpsink host=10.46.69.50 port=554 sync=false async=false \
+! omxh264enc control-rate=2 bitrate=40000 ! 'video/x-h264, stream-format=(strin$
+! h264parse\
+! rtph264pay\
+! udpsink host=10.46.69.5 port=554 \
 v4l2src device=/dev/video0 \
-! video/x-raw,width=640,height=480,framerate=30/1 \
+! 'video/x-raw,format=(string)I420,width=(int)640,height=(int)480,framerate=(fr$
 ! mix.sink_0 \
 v4l2src device=/dev/video1 \
-! video/x-raw,width=640,height=480,framerate=30/1 \
+! 'video/x-raw,format=(string)I420,width=(int)640,height=(int)480,framerate=(fr$
 ! mix.sink_1
